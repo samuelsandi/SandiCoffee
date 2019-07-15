5d01f8f240b3dcc87597c9d34d066bef82b5773a
Samuel Sandi Kristianto Lim <t-samuel.lim@ovo.id>
2019-07-15 14:05:36 +0700

    Basic view

---
+++ SandiCoffee/Controllers/AddOrderViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AddOrderViewController.swift
+//  SandiCoffee
+//
+//  Created by OVO on 15/07/19.
+//  Copyright © 2019 Samuel Lim. All rights reserved.
+//
+
+import UIKit
+
+class AddOrderViewController: UIViewController {
+    
+}
---
+++ SandiCoffee/Controllers/OrdersTableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  OrdersTableViewController.swift
+//  SandiCoffee
+//
+//  Created by OVO on 15/07/19.
+//  Copyright © 2019 Samuel Lim. All rights reserved.
+//
+
+import UIKit
+
+class OrdersTableViewController: UITableViewController {
+    
+}
