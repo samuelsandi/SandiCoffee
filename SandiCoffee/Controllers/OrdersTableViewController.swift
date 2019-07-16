1e78160841475e98dc9731b1b0cf840a8273b552
Samuel Sandi Kristianto Lim <t-samuel.lim@ovo.id>
2019-07-16 12:36:03 +0700

    Implementation of web service client

--- SandiCoffee/Controllers/OrdersTableViewController.swift
+++ SandiCoffee/Controllers/OrdersTableViewController.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class OrdersTableViewController: UITableViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        populateOrders()
+    }
     
+    private func populateOrders(){
+        guard let ordersURL = URL(string: "https://guarded-retreat-82533.herokuapp.com/orders") else {
+            fatalError("URL was incorrect")
+        }
+        let resource = Resource<[Order]>(url:ordersURL)
+        WebService().load(resource: resource) { result in
+            switch result {
+                case .success(let orders):
+                    print(orders)
+                case .failure(let error):
+                    print(error)
+            }
+        }
+    }
 }
